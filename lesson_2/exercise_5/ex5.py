67dad60e57afdb2d6c3952bad4695195bc4ad8c9
Caleb Delbrugge <caleb.delbrugge@gmail.com>
2023-06-22 17:32:07 -0700

    finishing ex5

--- lesson_2/exercise_5/ex5.py
+++ lesson_2/exercise_5/ex5.py
@@ -2,6 +2,7 @@ from nornir import InitNornir
 from nornir_netmiko import netmiko_send_command
 from nornir.core.filter import F
 from nornir_utils.plugins.functions import print_result
+import os
 
 nr = InitNornir(config_file="config.yaml")
 
@@ -15,14 +16,65 @@ results = nr.run(
     )
 
 print_result(results)
-print("-" * 50)
+print()
+print("=" * 50)
 print("Printing results.failed_hosts")
-print("-" * 50)
+print("=" * 50)
 print()
 print(results.failed_hosts)
-for host in results.failed_hosts:
-    print(host[0].result)
-print("Printing failed hostnames")
+print()
+print("=" * 50)
+print("Printing more details")
+print("=" * 50)
+print()
+for k,v in results.failed_hosts.items():
+    print("-" * 50)
+    print("Hostname")
+    print("-" * 50)
+    print()
+    print(k)
+    print()
+    print("-" * 50)
+    print("Result")
+    print("-" * 50)
+    print()
+    print(v[0].result)
+print("=" * 50)
+print("Printing nr.data.failed_hosts")
+print("=" * 50)
+print()
+print("-" * 50)
+print(nr.data.failed_hosts)
 print("-" * 50)
 print()
+
+#5c
+#nr.inventory.hosts["cisco3"].password = os.environ["NORNIR_PASSWORD"]
+#
+#results_again = nr.run(
+#    task=netmiko_send_command, 
+#    command_string="show ip int brief",
+#    on_good=False,
+#    on_failed=True
+#    )
+#
+#print_result(results_again)
+
+#5d
+print()
+print("=" * 50)
+print("Printing results.failed_hosts")
+print("=" * 50)
+print()
 print(nr.data.failed_hosts)
+print()
+print("Clearing failed host cisco3...")
+nr.data.recover_host("cisco3")
+print("Done")
+print()
+print("=" * 50)
+print("Printing results.failed_hosts")
+print("=" * 50)
+print()
+print(nr.data.failed_hosts)
+print()
